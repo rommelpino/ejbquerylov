aeadcc817078eef4845121e354d4e584a156b897
pino <rhpinocpa@gmail.com>
2013-04-29 14:29:03 +0300

    remove dependency to QueryLOV

--- EJBQueryLOV/src/soadev/ext/adf/query/SavedSearchDef.java
+++ EJBQueryLOV/src/soadev/ext/adf/query/SavedSearchDef.java
@@ -4,12 +4,16 @@ import java.io.Serializable;
 
 import java.util.ArrayList;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import java.util.Map;
 
 import oracle.adf.view.rich.model.ConjunctionCriterion;
-import static soadev.utils.MyUtils.*; 
+
+import static soadev.utils.MyUtils.*;
+
 import oracle.adf.view.rich.model.QueryDescriptor;
 
 
@@ -18,242 +22,240 @@ import oracle.adf.view.rich.model.QueryDescriptor;
  * Represents the definition for a saved search and contains a list of SearchFieldDef objects,
  * each SearchFieldDef being a kind of AttributeDef.
  */
-public class SavedSearchDef implements Serializable{
+public class SavedSearchDef implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    private QueryLOV _query;
-        private String _queryId;
-        // The default conjunction to use between all search fields. This is used in place of specific
-        // conjunction if one is not available
-        private ConjunctionCriterion.Conjunction _defaultConjunction = ConjunctionCriterion.Conjunction.AND;
-        // The name of the saved search. This also happens to be the display name.
-        private String _name;
-        // Whether the saved search is readOnly. A true value implies that the definition cannot be
-        // changed once its created.
-        private boolean _isReadOnly = false;
-        // A list of search fields associated to this SavedSearchDef
-        private List<SearchFieldDef> _searchFields;
-        // AutoExecute
-        private boolean _uiHintAutoExecute = false;
-        // Default
-        private boolean _uiHintDefault = false;
-        // ResultsId
-        private String _uiHintResultsId;
-        // Mode
-        private QueryDescriptor.QueryMode _uiHintMode = QueryDescriptor.QueryMode.ADVANCED;
-        // Show In List
-        private boolean _uiHintShowInList = true;
-        // Save Results Layout
-        private boolean _uiHintSaveResultsLayout = false;
-        // SavedSearchDef Id
-        private Long _id;
+//    private QueryLOV _query;
+    private String _queryId;
+    // The default conjunction to use between all search fields. This is used in place of specific
+    // conjunction if one is not available
+    private ConjunctionCriterion.Conjunction _defaultConjunction =
+        ConjunctionCriterion.Conjunction.AND;
+    // The name of the saved search. This also happens to be the display name.
+    private String _name;
+    // Whether the saved search is readOnly. A true value implies that the definition cannot be
+    // changed once its created.
+    private boolean _isReadOnly = false;
+    // A list of search fields associated to this SavedSearchDef
+    private List<SearchFieldDef> _searchFields;
+    // AutoExecute
+    private boolean _uiHintAutoExecute = false;
+    // Default
+    private boolean _uiHintDefault = false;
+    // ResultsId
+    private String _uiHintResultsId;
+    // Mode
+    private QueryDescriptor.QueryMode _uiHintMode =
+        QueryDescriptor.QueryMode.ADVANCED;
+    // Show In List
+    private boolean _uiHintShowInList = true;
+    // Save Results Layout
+    private boolean _uiHintSaveResultsLayout = false;
+    // SavedSearchDef Id
+    private Long _id;
 
-        public SavedSearchDef() {
+    public SavedSearchDef() {
 
+    }
+
+    public SavedSearchDef(String name, boolean autoExecute,
+                          QueryDescriptor.QueryMode mode,
+                          boolean saveResultsLayout, boolean showInList,
+                          boolean readOnly) {
+        this._name = name;
+        this._uiHintAutoExecute = autoExecute;
+        this._uiHintMode = mode;
+        this._uiHintSaveResultsLayout = saveResultsLayout;
+        this._uiHintShowInList = showInList;
+        this._isReadOnly = readOnly;
+    }
+
+    public SavedSearchDef copy() {
+        SavedSearchDef copy = new SavedSearchDef();
+        copy.setAutoExecute(this._uiHintAutoExecute);
+        copy.setDefaultSearch(this._uiHintDefault);
+        copy.setDefaultConjunction(this._defaultConjunction);
+        copy.setMode(this._uiHintMode);
+        copy.setName(this._name);
+        copy.setQueryId(this._queryId);
+        copy.setReadOnly(this._isReadOnly);
+        copy.setResultsId(this._uiHintResultsId);
+        copy.setSaveResultsLayout(this._uiHintSaveResultsLayout);
+        List<SearchFieldDef> searchFieldList = new ArrayList<SearchFieldDef>();
+        for (SearchFieldDef sfd : this._searchFields) {
+            searchFieldList.add(sfd.copy());
         }
+        copy.setSearchFields(searchFieldList);
+        copy.setShowInList(this._uiHintShowInList);
+        return copy;
+    }
 
-        public SavedSearchDef(String name, boolean autoExecute,
-                              QueryDescriptor.QueryMode mode,
-                              boolean saveResultsLayout, boolean showInList,
-                              boolean readOnly) {
-            _name = name;
-            _uiHintAutoExecute = autoExecute;
-            _uiHintMode = mode;
-            _uiHintSaveResultsLayout = saveResultsLayout;
-            _uiHintShowInList = showInList;
-            _isReadOnly = readOnly;
-        }
+    /**
+     * Adds a search field definition
+     * @param searchFieldDef
+     */
+    public void addSearchFieldDef(SearchFieldDef searchFieldDef) {
+        getSearchFields().add(searchFieldDef);
+//        searchFieldDef.setQuery(_query);
 
-      public SavedSearchDef copy(){
-            SavedSearchDef copy = new SavedSearchDef();
-            copy.setAutoExecute(this._uiHintAutoExecute);
-            copy.setDefaultSearch(this._uiHintDefault);
-            copy.setDefaultConjunction(this._defaultConjunction);
-            copy.setMode(this._uiHintMode);
-            copy.setName(this._name);
-            copy.setQuery(this._query);
-            copy.setQueryId(this._queryId);
-            copy.setReadOnly(this._isReadOnly);
-            copy.setResultsId(this._uiHintResultsId);
-            copy.setSaveResultsLayout(this._uiHintSaveResultsLayout);
-            List<SearchFieldDef> searchFieldList = new ArrayList<SearchFieldDef>();
-            for(SearchFieldDef sfd: this._searchFields){
-                searchFieldList.add(sfd.copy());
-            }
-            copy.setSearchFields(searchFieldList);
-            copy.setShowInList(this._uiHintShowInList);
-            return copy;
-        }
+    }
 
-        /**
-         * Adds a search field definition
-         * @param searchFieldDef
-         */
-        public void addSearchFieldDef(SearchFieldDef searchFieldDef) {
-            getSearchFields().add(searchFieldDef);
-            searchFieldDef.setQuery(_query);
-
-        }
-
-        /**
-         * Removes a search field definition
-         * @param searchFieldDef
-         */
-        public void removeSearchFieldDef(SearchFieldDef searchFieldDef) {
-            getSearchFields().remove(searchFieldDef);
-        }
+    /**
+     * Removes a search field definition
+     * @param searchFieldDef
+     */
+    public void removeSearchFieldDef(SearchFieldDef searchFieldDef) {
+        getSearchFields().remove(searchFieldDef);
+    }
 
 
-        /**
-         * Returns the default conjunction to use between all search fields. If a default is not set,
-         * this method loops through all its searchFieldDefs to see if they all use the same conjunction.
-         * If they do we use that otherwise this method returns ConjunctionType.NONE.
-         * @return
-         */
-        public ConjunctionCriterion.Conjunction getDefaultConjunction() {
-            boolean hasSameConj = true;
-            boolean bFirst = true;
+    /**
+     * Returns the default conjunction to use between all search fields. If a default is not set,
+     * this method loops through all its searchFieldDefs to see if they all use the same conjunction.
+     * If they do we use that otherwise this method returns ConjunctionType.NONE.
+     * @return
+     */
+    public ConjunctionCriterion.Conjunction getDefaultConjunction() {
+        boolean hasSameConj = true;
+        boolean bFirst = true;
 
-            ConjunctionCriterion.Conjunction prevConj =
-                ConjunctionCriterion.Conjunction.NONE;
-            for (SearchFieldDef field : getSearchFields()) {
-                ConjunctionCriterion.Conjunction currConj =
-                    field.getBeforeConjunction();
-                if (!bFirst) {
-                    // If the conjunction differs between search fields return NONE
-                    if (!prevConj.equals(currConj)) {
-                        hasSameConj = false;
-                        break;
-                    }
+        ConjunctionCriterion.Conjunction prevConj =
+            ConjunctionCriterion.Conjunction.NONE;
+        for (SearchFieldDef field : getSearchFields()) {
+            ConjunctionCriterion.Conjunction currConj =
+                field.getBeforeConjunction();
+            if (!bFirst) {
+                // If the conjunction differs between search fields return NONE
+                if (!prevConj.equals(currConj)) {
+                    hasSameConj = false;
+                    break;
                 }
-                prevConj = currConj;
-                bFirst = false;
             }
-
-            if (!hasSameConj) {
-                return ConjunctionCriterion.Conjunction.NONE;
-            } else {
-                return prevConj;
-            }
+            prevConj = currConj;
+            bFirst = false;
         }
 
-        public void setDefaultConjunction(ConjunctionCriterion.Conjunction conjunction) {
-            _defaultConjunction = conjunction;
+        if (!hasSameConj) {
+            return ConjunctionCriterion.Conjunction.NONE;
+        } else {
+            return prevConj;
         }
+    }
 
-        public void setName(String name) {
-            _name = name;
+    public void setDefaultConjunction(ConjunctionCriterion.Conjunction conjunction) {
+        _defaultConjunction = conjunction;
+    }
+
+    public void setName(String name) {
+        _name = name;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public String getResultsId() {
+        return _uiHintResultsId;
+    }
+
+    public List<SearchFieldDef> getSearchFields() {
+        if (_searchFields == null) {
+            _searchFields = new ArrayList<SearchFieldDef>();
         }
+        return _searchFields;
+    }
 
-        public String getName() {
-            return _name;
-        }
+    public boolean isReadOnly() {
+        return _isReadOnly;
+    }
 
-        public String getResultsId() {
-            return _uiHintResultsId;
-        }
+    public boolean isAutoExecute() {
+        return _uiHintAutoExecute;
+    }
 
-        public List<SearchFieldDef> getSearchFields() {
-            if (_searchFields == null) {
-                _searchFields = new ArrayList<SearchFieldDef>();
-            }
-            return _searchFields;
-        }
+    public QueryDescriptor.QueryMode getMode() {
+        return _uiHintMode;
+    }
 
-        public boolean isReadOnly() {
-            return _isReadOnly;
-        }
+    public boolean isDefaultSearch() {
+        return _uiHintDefault;
+    }
 
-        public boolean isAutoExecute() {
-            return _uiHintAutoExecute;
-        }
+    public boolean isShowInList() {
+        return _uiHintShowInList;
+    }
 
-        public QueryDescriptor.QueryMode getMode() {
-            return _uiHintMode;
-        }
+    public boolean isSaveResultsLayout() {
+        return _uiHintSaveResultsLayout;
+    }
 
-        public boolean isDefaultSearch() {
-            return _uiHintDefault;
-        }
+    public void setAutoExecute(boolean autoExecute) {
+        _uiHintAutoExecute = autoExecute;
+    }
 
-        public boolean isShowInList() {
-            return _uiHintShowInList;
-        }
+    /**
+     * Determines if the definition is readOnly and hence cannot be changed.
+     * @param readOnly
+     */
+    public void setReadOnly(boolean readOnly) {
+        _isReadOnly = readOnly;
+    }
 
-        public boolean isSaveResultsLayout() {
-            return _uiHintSaveResultsLayout;
-        }
+    public void setShowInList(boolean showInList) {
+        _uiHintShowInList = showInList;
+    }
 
-        public void setAutoExecute(boolean autoExecute) {
-            _uiHintAutoExecute = autoExecute;
-        }
+    public void setDefaultSearch(boolean isDefault) {
+        _uiHintDefault = isDefault;
+    }
 
-        /**
-         * Determines if the definition is readOnly and hence cannot be changed.
-         * @param readOnly
-         */
-        public void setReadOnly(boolean readOnly) {
-            _isReadOnly = readOnly;
-        }
+    public void setResultsId(String resultsId) {
+        _uiHintResultsId = resultsId;
+    }
 
-        public void setShowInList(boolean showInList) {
-            _uiHintShowInList = showInList;
-        }
+    public void setSaveResultsLayout(boolean saveLayout) {
+        _uiHintSaveResultsLayout = saveLayout;
+    }
 
-        public void setDefaultSearch(boolean isDefault) {
-            _uiHintDefault = isDefault;
-        }
+    public void setMode(QueryDescriptor.QueryMode mode) {
+        _uiHintMode = mode;
+    }
 
-        public void setResultsId(String resultsId) {
-            _uiHintResultsId = resultsId;
-        }
+    /**
+     *  Sets the Unique identifier for the SavedSearchDef
+     * @param _id
+     */
+    public void setId(Long _id) {
+        this._id = _id;
+    }
 
-        public void setSaveResultsLayout(boolean saveLayout) {
-            _uiHintSaveResultsLayout = saveLayout;
-        }
+    public Long getId() {
+        return _id;
+    }
 
-        public void setMode(QueryDescriptor.QueryMode mode) {
-            _uiHintMode = mode;
-        }
+    /**
+     * Sets the conjunction selected by the user for this saved search. This overrides the search
+     * field specific conjunction.
+     * @param selectedConjunction
+     */
+    //        public void setSelectedConjunction(ConjunctionCriterion.Conjunction selectedConjunction) {
+    //            this._selectedConjunction = selectedConjunction;
+    //        }
 
-        /**
-         *  Sets the Unique identifier for the SavedSearchDef
-         * @param _id
-         */
-        public void setId(Long _id) {
-            this._id = _id;
-        }
-
-        public Long getId() {
-            return _id;
-        }
-
-        /**
-         * Sets the conjunction selected by the user for this saved search. This overrides the search
-         * field specific conjunction.
-         * @param selectedConjunction
-         */
-//        public void setSelectedConjunction(ConjunctionCriterion.Conjunction selectedConjunction) {
-//            this._selectedConjunction = selectedConjunction;
+//    public Map<String, AttributeDef> getAttributes() {
+////        return getQuery().getAttributes();
+//        if(_attributes == null){
+//            return Collections.EMPTY_MAP;
 //        }
-
-    public Map<String, AttributeDef> getAttributes() {
-        return getQuery().getAttributes();
-    }
-    
-    public void setQuery(QueryLOV query) {
-        this._query = query;
-        for(SearchFieldDef sfd: getSearchFields()){
-            sfd.setQuery(query);
-        }
-    }
-
-    public QueryLOV getQuery() {
-        return _query;
-    }
+//        return _attributes;
+//    }
+//    
+//    public void setAttributes(Map<String, AttributeDef> attributes) {
+//        this._attributes = attributes;
+//    }
 
     public void setSearchFields(List<SearchFieldDef> searchFields) {
-        for(SearchFieldDef sfd: searchFields){
+        for (SearchFieldDef sfd : searchFields) {
             addSearchFieldDef(sfd);
         }
     }
@@ -265,31 +267,32 @@ public class SavedSearchDef implements Serializable{
     public String getQueryId() {
         return _queryId;
     }
-    
-    public void update(Map<String, Object> uiHints){
-        for (Map.Entry<String, Object> entry: uiHints.entrySet()){
-            if (entry.getKey().equals(QueryDescriptor.UIHINT_NAME)){
+
+    public void update(Map<String, Object> uiHints) {
+        for (Map.Entry<String, Object> entry : uiHints.entrySet()) {
+            if (entry.getKey().equals(QueryDescriptor.UIHINT_NAME)) {
                 setName((String)entry.getValue());
             }
-            if (entry.getKey().equals(QueryDescriptor.UIHINT_DEFAULT)){
+            if (entry.getKey().equals(QueryDescriptor.UIHINT_DEFAULT)) {
                 setDefaultSearch(unbox((Boolean)entry.getValue()));
             }
-            if (entry.getKey().equals(QueryDescriptor.UIHINT_AUTO_EXECUTE)){
+            if (entry.getKey().equals(QueryDescriptor.UIHINT_AUTO_EXECUTE)) {
                 setAutoExecute(unbox((Boolean)entry.getValue()));
             }
-            if (entry.getKey().equals(QueryDescriptor.UIHINT_RESULTS_COMPONENT_ID)){
+            if (entry.getKey().equals(QueryDescriptor.UIHINT_RESULTS_COMPONENT_ID)) {
                 setResultsId((String)entry.getValue());
             }
-//            if (entry.getKey().equals(QueryDescriptor.UIHINT_SHOW_IN_LIST)){
-//                setShowInList(unbox((Boolean)entry.getValue()));
-//            }
-            if (entry.getKey().equals(QueryDescriptor.UIHINT_SAVE_RESULTS_LAYOUT)){
+            //            if (entry.getKey().equals(QueryDescriptor.UIHINT_SHOW_IN_LIST)){
+            //                setShowInList(unbox((Boolean)entry.getValue()));
+            //            }
+            if (entry.getKey().equals(QueryDescriptor.UIHINT_SAVE_RESULTS_LAYOUT)) {
                 setSaveResultsLayout(unbox((Boolean)entry.getValue()));
             }
-            
-            
-            
+
+
         }
     }
+
+
 }
 
--- EJBQueryLOV/src/soadev/ext/adf/query/SearchFieldDef.java
+++ EJBQueryLOV/src/soadev/ext/adf/query/SearchFieldDef.java
@@ -14,182 +14,165 @@ import oracle.adf.view.rich.model.ConjunctionCriterion;
  * info such as the default operator to use for the attribute when it is used is a search
  * criteria etc.
  */
-    public class SearchFieldDef implements Cloneable, Serializable{
+public class SearchFieldDef implements Cloneable, Serializable {
 
     private static final long serialVersionUID = 1L;
-    private QueryLOV _query;
 
-        // The unique identifying field for SearchFieldDef
-        private Long _id;
+    // The unique identifying field for SearchFieldDef
+    private Long _id;
 
-        // the name of the attribute that this search field references
-        private String _attrName;
-        // The conjunction to use before this search field.
-        private ConjunctionCriterion.Conjunction _beforeConjunction = ConjunctionCriterion.Conjunction.AND;
-        // default operator to use with this search field
-        private OperatorDef _operator = OperatorDef.NO_OPERATOR;
+    // the name of the attribute that this search field references
+    private String _attrName;
+    // The conjunction to use before this search field.
+    private ConjunctionCriterion.Conjunction _beforeConjunction =
+        ConjunctionCriterion.Conjunction.AND;
+    // default operator to use with this search field
+    private OperatorDef _operator;
 
-        // if the search field has dependent fields
-        private boolean[] _hasDependentFields = new boolean[] { false, false };
+    // if the search field has dependent fields
+    private boolean[] _hasDependentFields = new boolean[] { false, false };
 
-        // the entered values. it could be a String literal or a Number
-        private List _values;
-        private boolean _isRemovable = true;
-        private boolean _isMultiSelect = false;
+    // the entered values. it could be a String literal or a Number
+    private List _values;
+    private boolean _isRemovable = true;
+    private boolean _isMultiSelect = false;
 
-        public SearchFieldDef() {
+    public SearchFieldDef() {
 
-        }
-
- 
-        public SearchFieldDef copy(){
-            SearchFieldDef copy = new SearchFieldDef();
-            copy.setAttrName(this._attrName);
-            copy.setBeforeConjunction(this._beforeConjunction);
-            copy.setOperator(this._operator);
-            copy.setHasDependentFields(this._hasDependentFields);
-            copy.setId(this._id);
-            copy.setMultiSelect(this._isMultiSelect);
-            copy.setQuery(this._query);
-            copy.setRemovable(this._isRemovable);
-            copy.setValues(this._values);
-            return copy;
-        }
+    }
 
 
-        public AttributeDef getAttributeDef() {
-            return getAttributeDescriptor(_attrName);
-        }
+    public SearchFieldDef copy() {
+        SearchFieldDef copy = new SearchFieldDef();
+        copy.setAttrName(this._attrName);
+        copy.setBeforeConjunction(this._beforeConjunction);
+        copy.setOperator(this._operator);
+        copy.setHasDependentFields(this._hasDependentFields);
+        copy.setId(this._id);
+        copy.setMultiSelect(this._isMultiSelect);
+        copy.setRemovable(this._isRemovable);
+        copy.setValues(this._values);
+        return copy;
+    }
 
-        public boolean equals(String attrName) {
-            if (attrName != null)
-                return (this.getAttribute().equals(attrName));
-            return false;
-        }
+    public boolean equals(String attrName) {
+        if (attrName != null)
+            return (this.getAttribute().equals(attrName));
+        return false;
+    }
 
-        /**
-         * Gets the name of the attribute
-         * @return
-         */
-        public String getAttribute() {
-            return _attrName;
-        }
+    /**
+     * Gets the name of the attribute
+     * @return
+     */
+    public String getAttribute() {
+        return _attrName;
+    }
 
-        /**
-         * Gets the conjunction to use before this search field.
-         * @return
-         */
-        public ConjunctionCriterion.Conjunction getBeforeConjunction() {
-            return _beforeConjunction;
-        }
+    /**
+     * Gets the conjunction to use before this search field.
+     * @return
+     */
+    public ConjunctionCriterion.Conjunction getBeforeConjunction() {
+        return _beforeConjunction;
+    }
 
-        /**
+    /**
          * Gets the selected (if changed by user) or the default operator to use with this search field
          * @return
          */
 
-        /**
-         * Returns the list of operators for this search field. Basically it loops through the supported
-         * list of operators for the attribute and replace Equals and Not Equals with In and Not In, for
-         * LOV attribute types
-         * @return a List&lt;OperatorDef&gt;
-         */
-        public final List<OperatorDef> getOperators() {
-            return getAttributeDef().getSupportedOperators();
-        }
+    /**
+     * Returns the list of operators for this search field. Basically it loops through the supported
+     * list of operators for the attribute and replace Equals and Not Equals with In and Not In, for
+     * LOV attribute types
+     * @return a List&lt;OperatorDef&gt;
+     */
+//    public final List<OperatorDef> getOperators() {
+//        return getAttributeDef().getSupportedOperators();
+//    }
 
 
-        public OperatorDef getOperator() {
-            return _operator;
-        }
-
-        public boolean isRemovable() {
-            return _isRemovable;
-        }
-
-        /**
-         * Gets the last saved values.
-         * @return
-         */
-        public List getValues() {
-            if (_values == null) {
-                _values = new ArrayList();
-            }
-            return _values;
-        }
-
-
-        public void setRemovable(boolean removable) {
-            _isRemovable = removable;
-        }
-
-        /**
-         * Returns true, if this criterion has dependent field for given value map index.
-         * @param index the value map index of the search value field
-         * @return boolean
-         */
-        public boolean hasDependentField(int index) {
-            return _hasDependentFields[index];
-        }
-
-        public boolean[] getHasDependentFields() {
-            return _hasDependentFields;
-        }
-
-        public boolean isMultiSelect() {
-            return _isMultiSelect;
-        }
-
-        /**
-         * Sets the unique identifier for SearchFieldDef
-         * @param _id
-         */
-        public void setId(Long _id) {
-            this._id = _id;
-        }
-
-        /**
-         *
-         * @return returns the unique identifier for SearchFieldDef
-         */
-        public Long getId() {
-            return _id;
-        }
-
-
-        public void setAttrName(String attrName) {
-            this._attrName = attrName;
-        }
-
-        public void setBeforeConjunction(ConjunctionCriterion.Conjunction beforeConjunction) {
-            this._beforeConjunction = beforeConjunction;
-        }
-
-        public void setMultiSelect(boolean isMultiSelect) {
-            this._isMultiSelect = isMultiSelect;
-        }
-
-        public void setOperator(OperatorDef operator) {
-            this._operator = operator;
-        }
-
-        public void setHasDependentFields(boolean[] hasDependentFields) {
-            this._hasDependentFields = hasDependentFields;
-        }
-
-        public void setValues(List values) {
-            this._values = values;
-        }
-
-    private AttributeDef getAttributeDescriptor(String attrName) {
-        return getQuery().getAttributeDescriptor(attrName);
+    public OperatorDef getOperator() {
+        return _operator;
     }
 
-    public void setQuery(QueryLOV _query) {
-        this._query = _query;
+    public boolean isRemovable() {
+        return _isRemovable;
     }
 
-    public QueryLOV getQuery() {
-        return _query;
+    /**
+     * Gets the last saved values.
+     * @return
+     */
+    public List getValues() {
+        if (_values == null) {
+            _values = new ArrayList();
+        }
+        return _values;
     }
+
+
+    public void setRemovable(boolean removable) {
+        _isRemovable = removable;
+    }
+
+    /**
+     * Returns true, if this criterion has dependent field for given value map index.
+     * @param index the value map index of the search value field
+     * @return boolean
+     */
+    public boolean hasDependentField(int index) {
+        return _hasDependentFields[index];
+    }
+
+    public boolean[] getHasDependentFields() {
+        return _hasDependentFields;
+    }
+
+    public boolean isMultiSelect() {
+        return _isMultiSelect;
+    }
+
+    /**
+     * Sets the unique identifier for SearchFieldDef
+     * @param _id
+     */
+    public void setId(Long _id) {
+        this._id = _id;
+    }
+
+    /**
+     *
+     * @return returns the unique identifier for SearchFieldDef
+     */
+    public Long getId() {
+        return _id;
+    }
+
+
+    public void setAttrName(String attrName) {
+        this._attrName = attrName;
+    }
+
+    public void setBeforeConjunction(ConjunctionCriterion.Conjunction beforeConjunction) {
+        this._beforeConjunction = beforeConjunction;
+    }
+
+    public void setMultiSelect(boolean isMultiSelect) {
+        this._isMultiSelect = isMultiSelect;
+    }
+
+    public void setOperator(OperatorDef operator) {
+        this._operator = operator;
+    }
+
+    public void setHasDependentFields(boolean[] hasDependentFields) {
+        this._hasDependentFields = hasDependentFields;
+    }
+
+    public void setValues(List values) {
+        this._values = values;
+    }
+
 }
